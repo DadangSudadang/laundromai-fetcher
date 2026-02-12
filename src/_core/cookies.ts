f0ae1aaf1b3663b76caf0165397817d896e7af98
Dadang Sudadang <dsudadang30@gmail.com>
2026-02-13 00:10:56 +0900

    finishing cookies.ts

---
+++ src/_core/cookies.ts
@@ -0,0 +1,100 @@
+import log4js from "log4js";
+import puppeteer from "puppeteer";
+
+import dotenvFlow from 'dotenv-flow';
+dotenvFlow.config();
+
+const logger = log4js.getLogger("cookies");
+logger.level = log4js.levels.INFO;
+
+const JP_SITE = "https://maimaidx.jp/maimai-mobile";
+const INTL_SITE = "https://maimaidx-eng.com/maimai-mobile"
+
+// ----
+// Logs in to Japan maimaiNET by simulating clicks to get the login cookie.
+// Returns cookie key-value pairs
+// ----
+export async function getJpCookies() {
+	if (!process.env.MAIMAI_JP_SEGA_ID || !process.env.MAIMAI_JP_SEGA_PASSWORD) {
+		throw new Error(
+			"Please set your MAIMAI_JP_SEGA_ID and MAIMAI_JP_SEGA_PASSWORD in the .env file",
+		);
+	}
+
+    logger.info("Logging in JP maimaiNET...")
+
+	const browser = await puppeteer.launch();
+
+	const page = await browser.newPage();
+	await page.goto(JP_SITE);
+
+	await page.type('input[name="segaId"]', process.env.MAIMAI_JP_SEGA_ID);
+	await page.type(
+		'input[name="password"]',
+		process.env.MAIMAI_JP_SEGA_PASSWORD,
+	);
+
+	await Promise.all([
+		page.waitForNavigation(),
+		page.click(
+			`form[action="${JP_SITE}/submit/"] button[type="submit"]`,
+		),
+	]);
+
+	await Promise.all([
+		page.waitForNavigation(),
+		page.click(
+			`form[action="${JP_SITE}/aimeList/submit/"] button[type="submit"]`,
+		),
+	]);
+
+	const cookies = await browser.cookies();
+	await browser.close();
+
+    logger.info("Successfully fetched cookies.")
+	return Object.fromEntries(
+		cookies.map((cookie) => [cookie.name, cookie.value]),
+	);
+}
+
+// ----
+// Logs in to International maimaiNET by simulating clicks to get the login cookie.
+// Returns cookie key-value pairs
+// ----
+export async function getIntlCookies() {
+    if (!process.env.MAIMAI_INTL_SEGA_ID || !process.env.MAIMAI_INTL_SEGA_PASSWORD) {
+        throw new Error('Please set your MAIMAI_INTL_SEGA_ID and MAIMAI_INTL_SEGA_PASSWORD in the .env file');
+    }
+
+    if (!process.env.USER_AGENT) {
+        throw new Error('Please set a valid USER_AGENT in the .env file');
+    }
+
+    logger.info("Logging in INTL maimaiNET...")
+
+    const browser = await puppeteer.launch();
+    const url = new URL('https://lng-tgk-aime-gw.am-all.net/common_auth/login');
+    url.searchParams.set('site_id', 'maimaidxex');
+    url.searchParams.set('redirect_url', INTL_SITE);
+
+    const page = await browser.newPage();
+    await page.setUserAgent(process.env.USER_AGENT);
+
+    await page.goto(url.toString());
+
+    await page.click('#agree-maimaidxex #agree');
+    await page.click('.c-button--openid--segaId');
+    await page.type('#sid', process.env.MAIMAI_INTL_SEGA_ID);
+    await page.type('#password', process.env.MAIMAI_INTL_SEGA_PASSWORD);
+
+    await Promise.all([
+        page.waitForNavigation(),
+        page.click('#btnSubmit'),
+    ]);
+
+    const cookies = await browser.cookies();
+    await browser.close();
+    
+    logger.info("Successfully fetched cookies.")
+    return Object.fromEntries(cookies.map((cookie) => [cookie.name, cookie.value]));
+} 
