59227326636f6b5839d8f24c31bb156c37d9a834
Dadang Sudadang <dsudadang30@gmail.com>
2026-05-29 01:12:50 +0900

    new-songs: add timer, move and rename script

---
+++ src/scripts/fetch-new-songs.ts
@@ -0,0 +1,40 @@
+import { getUserId } from "@_core/cookies";
+import { fetchGenreList } from "@fetch/fetch-genre";
+import { fetchNewSongs } from "@fetch/fetch-new-songs";
+import sleep from "sleep-promise";
+import fs from "fs";
+
+const region = "jp"
+let userId: string;
+
+async function fetchNewMasters() {
+    const pmasList = JSON.parse(
+        fs.readFileSync("data/latest/master.json", "utf-8"),
+    )
+    const masList = await fetchGenreList("master", region, userId)
+    fetchNewSongs("master", pmasList, masList, region, userId)
+}
+
+async function fetchNewUtage() {
+    const putgList = JSON.parse(
+        fs.readFileSync("data/latest/utage.json", "utf-8"),
+    )
+    const utgList = await fetchGenreList("utage", "jp", userId)
+    fetchNewSongs("utage", putgList, utgList, "jp", userId)
+}
+
+async function timerTest() {
+    const now = new Date(); 
+    const target = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 7, 0, 3, 0);
+    const millisTilTarget =  target.getTime() - now.getTime();
+    
+    if (millisTilTarget > 0) {
+        console.log(`Waiting until ${target.toLocaleTimeString()}...`);
+        await sleep(millisTilTarget);
+    }
+}
+
+await timerTest();
+userId = await getUserId(region)
+await fetchNewMasters();
+await fetchNewUtage();
