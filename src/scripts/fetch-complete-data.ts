44aa5cbb86bf5d0a2d0abf0838afc445644a3527
Dadang Sudadang <dsudadang30@gmail.com>
2026-09-23 11:10:31 +0900

    fix: fetch-complete-data: forgot to add in userId back

--- src/scripts/fetch-complete-data.ts
+++ src/scripts/fetch-complete-data.ts
@@ -8,12 +8,7 @@ import { fetchCombinedData } from "@fetch/fetch-combined-data";
 const userId = await getUserId("jp");
 const genreList = await fetchGenreList("master", "jp", userId) as chartGenreInterface[];
 try {
-    // Uncomment this if you wish to load an existing file instead.
-    // const genreList: chartGenreInterface[] = JSON.parse(
-    //     fs.readFileSync('./dist/genre/master.json', 'utf-8')
-    // )
-
-    fetchCombinedData(genreList, "jp", "");
+    fetchCombinedData(genreList, "jp", userId);
 } catch (e) {
     console.log(e)
 }
