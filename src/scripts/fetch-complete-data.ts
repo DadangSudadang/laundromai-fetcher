0bc68bba83bb6d750e0ff1a950afd91c7f6308d7
Dadang Sudadang <dsudadang30@gmail.com>
2026-09-23 11:11:40 +0900

    refactor: fetch-complete-data

--- src/scripts/fetch-complete-data.ts
+++ src/scripts/fetch-complete-data.ts
@@ -1,7 +1,5 @@
-import fs from 'fs';
 import { getUserId } from "@_core/cookies";
 import { fetchGenreList } from "@fetch/fetch-genre";
-import { fetchConstantsList, fetchAllConstants } from "@fetch/fetch-constants";
 import { chartGenreInterface } from "@_core/types";
 import { fetchCombinedData } from "@fetch/fetch-combined-data";
 
